7a3c7caf474aafe2b989b976e2e6b5d8f81cf35d
John Anderson <jander@plurr.org>
2015-05-26 21:32:23 -0400

    Create packet classes and read/write functions
    Same functionality as before: read the first three blocks of flight
    recording data.

--- aim-usb.py
+++ aim-usb.py
@@ -3,6 +3,83 @@
 import usb.core
 import usb.util
 
+
+class PType(object):
+    DATA            = 0x0
+    RESET           = 0x1
+    READ_BLOCK      = 0x2
+    READ_PRESSURE   = 0x3
+    FAKE_LAUNCH     = 0x4
+    COMPLETE        = 0x5
+    ERASE_MEM       = 0x6
+    READ_BAT        = 0x7
+    READ_SETTINGS   = 0x8
+    WRITE_SETTINGS  = 0x9
+    FIRE_LINES      = 0xa
+    RECORDING_DATA  = 0xb
+
+    def __init__(self, t):
+        if t < 0 or t > 0xb:
+            raise ValueError("Valid PType values are 0..11")
+        self.t = int(t)
+
+    def __int__(self):
+        return self.t
+
+    def __eq__(self, t):
+        return self.t == int(t)
+
+    def __ne__(self, t):
+        return self.t != int(t)
+
+    def value(self):
+        return self.t
+
+
+class Packet(object):
+    def __init__(self, data, t=None):
+        if t is None:
+            if len(data) == 3:
+                self.v = (int(data[1]) << 8) + int(data[0])
+                self.t = PType(data[2])
+            elif len(data) == 2:
+                self.v = int(data[0])
+                self.t = PType(data[1])
+            else:
+                raise ValueError("Bad arguments to Packet()");
+        else:
+            self.v = int(data) & 0xffff
+            self.t = PType(t)
+
+    def raw(self):
+        return [self.v & 0xff, (self.v >> 8) & 0xff, int(self.t) & 0xff]
+
+    def __str__(self):
+        return str(self.raw())
+
+
+########
+
+def write_packet(dev, packet):
+    print '  >>> WRITE ', packet
+    dev.write(0x01, packet.raw(), 100)
+
+def read_packet(dev, timeout=2500):   # some responses take awhile
+    s = dev.read(0x81, 4, timeout)
+    assert(len(s) == 3)
+    p = Packet(s)
+    print '  <<< READ ', p
+    return p
+
+def clear_read_buffer(dev):
+    while True:
+        try:
+            read_packet(dev, timeout=100)
+        except:
+            break
+
+########
+
 dev = usb.core.find(idVendor=0x3db, idProduct=0x0002)
 
 if dev is None:
@@ -14,30 +91,12 @@ if dev is None:
 
 # ep = usb.util.find_descriptor(intf,
 
-still=True
-while still:
-    try:
-        dev.read(0x81, 5, 100)
-    except:
-        still=False
-
-dev.write(0x01, '\x00\x00\x01', 100)
-
+clear_read_buffer(dev)
+write_packet(dev, Packet(0, PType.RESET))
 for i in xrange(0, 3):
-    dev.write(0x01, '\x00\x00\x02', 100)
-    print '  >>> WRITE 00 00 02'
-#    reply=None
-#    while reply is None or reply[2] != 0x05:
-#        try:
-#            reply = dev.read(0x81, 5, 2500)     # some responses take awhile
-#        except:
-#            break
-#        print ' '.join('{0:02x}'.format(b) for b in reply)
-    dev.write(0x01, [i, 0, 2], 100)
-    print '  >>> WRITE {0:02x} 00 02'.format(i)
+    write_packet(dev, Packet(0, PType.READ_BLOCK))
+    write_packet(dev, Packet(i, PType.READ_BLOCK))
     reply=None
-    while reply is None or reply[2] != 0x05:
-        reply = dev.read(0x81, 5, 2500)     # some responses take awhile
-        print ' '.join('{0:02x}'.format(b) for b in reply)
-
+    while reply is None or reply.t != PType.COMPLETE:
+        reply = read_packet(dev)
 
